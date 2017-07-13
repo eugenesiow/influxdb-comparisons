14181040ceb6a0ffb5762ba3b88469ca603e96dd
Eugene Siow <kyo116@gmail.com>
2017-07-13 17:08:05 +0100

    Update generate and load scripts

--- cmd/generate_data_iot.sh
+++ cmd/generate_data_iot.sh
@@ -1,3 +1,13 @@
 echo "Generate data for InfluxDB"
-./bulk_data_transform/bulk_data_transform --use-case=shelburne --format=influx-bulk --input="/scratch/shelburne.csv" | gzip > influx_shelburne.gz
-./bulk_data_transform/bulk_data_transform --use-case=green_taxi --format=influx-bulk --input="/scratch/2016_green_taxi_trip_data.csv" | gzip > influx_green_taxi.gz
+#./bulk_data_transform/bulk_data_transform --use-case=green_taxi --format=influx-bulk --input="/scratch/2016_green_taxi_trip_data.csv" | gzip > influx_green_taxi.gz
+#./bulk_data_transform/bulk_data_transform --use-case=green_taxi --format=mongo --input="/scratch/2016_green_taxi_trip_data.csv" | gzip > mongo_green_taxi.gz
+#./bulk_data_transform/bulk_data_transform --use-case=shelburne --format=mongo --input="/scratch/shelburne.csv" | gzip > mongo_shelburne.gz
+#./bulk_data_transform/bulk_data_transform --use-case=srbench --format=mongo --input="/scratch/knoesis_observations_csv_date_sorted/" | gzip > mongo_srbench.gz
+#./bulk_data_transform/bulk_data_transform --use-case=srbench --format=cassandra --input="/scratch/knoesis_observations_csv_date_sorted/" | gzip > cassandra_srbench.gz
+#./bulk_data_transform/bulk_data_transform --use-case=shelburne --format=influx-bulk --input="/scratch/shelburne.csv" | gzip > influx_shelburne.gz
+#./bulk_data_transform/bulk_data_transform --use-case=shelburne --format=cassandra --input="/scratch/shelburne.csv" | gzip > cassandra_shelburne.gz
+./bulk_data_transform/bulk_data_transform --use-case=shelburne --format=opentsdb --input="/scratch/shelburne.csv" | gzip > opentsdb_shelburne.gz
+#./bulk_data_transform/bulk_data_transform --use-case=srbench --format=influx-bulk --input="/scratch/knoesis_observations_csv_date_sorted/" | gzip > influx_srbench.gz
+#./bulk_data_transform/bulk_data_transform --use-case=srbench --format=opentsdb --input="/scratch/knoesis_observations_csv_date_sorted/" | gzip > opentsdb_srbench.gz
+#./bulk_data_transform/bulk_data_transform --use-case=green_taxi --format=cassandra --input="/scratch/2016_green_taxi_trip_data.csv" | gzip > cassandra_green_taxi.gz
+#./bulk_data_transform/bulk_data_transform --use-case=green_taxi --format=opentsdb --input="/scratch/2016_green_taxi_trip_data.csv" | gzip > opentsdb_green_taxi.gz
--- cmd/load_data_iot.sh
+++ cmd/load_data_iot.sh
@@ -3,7 +3,29 @@
 # Load data
 
 INFLUX_SHELBURNE_GZ=/scratch/influx_shelburne.gz
+INFLUX_SRBENCH_GZ=/scratch/influx_srbench.gz
+INFLUX_GREEN_TAXI_GZ=/scratch/influx_green_taxi.gz
+MONGO_GREEN_TAXI_GZ=/scratch/mongo_green_taxi.gz
+MONGO_SHELBURNE_GZ=/scratch/mongo_shelburne.gz
+MONGO_SRBENCH_GZ=/scratch/mongo_srbench.gz
+CASSANDRA_SRBENCH_GZ=/scratch/cassandra_srbench.gz
+CASSANDRA_SHELBURNE_GZ=/scratch/cassandra_shelburne.gz
+CASSANDRA_GREEN_TAXI_GZ=/scratch/cassandra_green_taxi.gz
+OPENTSDB_GREEN_TAXI_GZ=/scratch/opentsdb_green_taxi.gz
+OPENTSDB_SRBENCH_GZ=/scratch/opentsdb_srbench.gz
+OPENTSDB_SHELBURNE_GZ=/scratch/opentsdb_shelburne.gz
+
 
 echo "Loading data into InfluxDB"
 
-cat $INFLUX_SHELBURNE_GZ | gunzip | ./bulk_load_influx/bulk_load_influx --batch-size=250 --workers=1
+#cat $MONGO_GREEN_TAXI_GZ | gunzip | ./bulk_load_mongo/bulk_load_mongo --batch-size=1 --workers=4
+#cat $MONGO_SHELBURNE_GZ | gunzip | ./bulk_load_mongo/bulk_load_mongo --batch-size=1 --workers=4
+#cat $MONGO_SRBENCH_GZ | gunzip | ./bulk_load_mongo/bulk_load_mongo --batch-size=1 --workers=4
+#cat $INFLUX_GREEN_TAXI_GZ | gunzip | ./bulk_load_influx/bulk_load_influx --batch-size=1 --workers=4
+#cat $INFLUX_SRBENCH_GZ | gunzip | ./bulk_load_influx/bulk_load_influx --batch-size=1 --workers=4
+cat $OPENTSDB_SHELBURNE_GZ | gunzip | ./bulk_load_opentsdb/bulk_load_opentsdb --batch-size=1 --workers=4
+#cat $OPENTSDB_SRBENCH_GZ | gunzip | ./bulk_load_opentsdb/bulk_load_opentsdb --batch-size=1 --workers=4
+#cat $CASSANDRA_SRBENCH_GZ | gunzip | ./bulk_load_cassandra/bulk_load_cassandra --batch-size=1 --workers=4
+#cat $CASSANDRA_SHELBURNE_GZ | gunzip | ./bulk_load_cassandra/bulk_load_cassandra --batch-size=1 --workers=4
+#cat $CASSANDRA_GREEN_TAXI_GZ | gunzip | ./bulk_load_cassandra/bulk_load_cassandra --batch-size=1 --workers=4
+#cat $OPENTSDB_GREEN_TAXI_GZ | gunzip | ./bulk_load_opentsdb/bulk_load_opentsdb --batch-size=1 --workers=8
